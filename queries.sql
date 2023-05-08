c2b12af93c7f684a5a27638ce9103113850831f6
saluumaa <salumahassan02@gmail.com>
2023-05-08 18:49:33 +0300

    Make escape attempst less than three

--- queries.sql
+++ queries.sql
@@ -1,5 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
-
 SELECT * from animals WHERE name = 'Luna';
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-30';
