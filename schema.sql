c1697d7766db88a2f4eaab811c22b00c2db0b887
saluumaa <salumahassan02@gmail.com>
2023-05-11 17:14:28 +0300

    Create new Tables

--- schema.sql
+++ schema.sql
@@ -39,3 +39,25 @@ ALTER TABLE animals ADD COLUMN species_id INT;
 ALTER TABLE animals ADD CONSTRAINT FK_species FOREIGN KEY(species_id) REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT FK_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+-- /* RELATIONS */
+
+CREATE TABLE vets (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(100),
+  age INT,
+  date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  vet_id INT REFERENCES vets(id),
+  species_id INT REFERENCES species(id)
+);
+
+CREATE TABLE visits(
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+vet_id INT REFERENCES vets(id),
+animals_id INT REFERENCES animals(id),
+visit_date DATE
+);
