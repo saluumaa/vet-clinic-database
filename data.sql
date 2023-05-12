47e75a4feda1e44823eaec4d69528ac2179b87d9
Salma Ibrahim <salumahassan02@gmail.com>
2023-05-12 09:48:33 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -73,8 +73,8 @@ WHERE name IN ('Angemon', 'Boarmon');
 
 INSERT INTO vets (name, age, date_of_graduation)
 VALUES ('William Tatcher', 45, '2000-04-23'),
-       ('Maisy Smith', 26, '2019-01-17');
-       ('Stephanie Mendez', 64, '1981-05-04');
+       ('Maisy Smith', 26, '2019-01-17'),
+       ('Stephanie Mendez', 64, '1981-05-04'),
        ('Jack Harkness', 38, '2008-06-08');
 
 INSERT INTO specialization(vet_id, species_id)
